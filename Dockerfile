171016e84c6fc0475414ab1f0c98f2fb674986e3
Théo Willain <theo.willain@outlook.com>
2024-12-10 10:05:40 +0100

    Dockerize the app and the db

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM python:3.11-slim
+FROM python:3.10-slim
 
-WORKDIR /app
+# Set a directory for the app
+WORKDIR /usr/src/app
 
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
+# Copy all the files to the container
 COPY . .
 
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Define the port number the container should expose
+EXPOSE 8000
+
+# Run the FastAPI project
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#DATABASE_URL = "postgresql+asyncpg://user:password@localhost/production_db"
-DATABASE_URL = "postgresql+asyncpg://admin:o4vHj7ZXBOVpA1b6WOARHV0swc40MORC@dpg-ct8r9u56l47c73d6ksk0-a.frankfurt-postgres.render.com/devops_1v6d"
+DATABASE_URL = "postgresql+asyncpg://fastapi_user:secret_password@postgres-db/fastapi_db"
+#DATABASE_URL = "postgresql+asyncpg://admin:o4vHj7ZXBOVpA1b6WOARHV0swc40MORC@dpg-ct8r9u56l47c73d6ksk0-a.frankfurt-postgres.render.com/devops_1v6d"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
